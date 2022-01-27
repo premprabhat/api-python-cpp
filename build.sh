003d4497bc14c939296e7170786a57ad62203234
premprabhat <86395957+premprabhat@users.noreply.github.com>
2022-01-27 16:53:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf build/*
 rm -rf wheel/*
-docker run --rm --privileged hypriot/qemu-register
-docker run --rm -e PLAT=manylinux2014_aarch64 -v `pwd`:/io quay.io/pypa/manylinux2014_aarch64 /io/build-wheels.sh
-docker run --rm -e PLAT=manylinux2010_x86_64 -v `pwd`:/io dev-ssl /io/build-wheels.sh
+#docker run --rm --privileged hypriot/qemu-register
+#docker run --rm -e PLAT=manylinux2014_aarch64 -v `pwd`:/io quay.io/pypa/manylinux2014_aarch64 /io/build-wheels.sh
+docker run --rm -e PLAT=manylinux2010_x86_64 -v `pwd`:/io webzuu/dev-ssl /io/build-wheels.sh
